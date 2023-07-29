35768e365e2e05126d591ca452ec54760ea99b30
raspi <pekka.jarvinen@gmail.com>
2023-07-29 17:54:11 +0300

    comments and some cleanup

--- internal/playlist/result.go
+++ internal/playlist/result.go
@@ -7,38 +7,38 @@ import (
 
 type Result struct {
 	//Kind          string          `json:"kind"`
-	Etag          string          `json:"etag"` // for caching
-	NextPageToken *string         `json:"nextPageToken,omitempty"`
-	PageInfo      shared.PageInfo `json:"pageInfo"`
-	Items         []Item          `json:"items"`
+	Etag          string          `json:"etag"`                    // for caching
+	NextPageToken *string         `json:"nextPageToken,omitempty"` // for `pageToken` parameter
+	PageInfo      shared.PageInfo `json:"pageInfo"`                // How many results etc
+	Items         []Item          `json:"items"`                   // Playlist
 }
 
 type Item struct {
 	//Kind           string           `json:"kind"`
 	Etag           string         `json:"etag"` // for caching
-	ID             string         `json:"id"`
+	ID             string         `json:"id"`   // Playlist ID
 	Snippet        Snippet        `json:"snippet"`
 	Status         Status         `json:"status"`
-	ContentDetails ContentDetails `json:"contentDetails"`
+	ContentDetails ContentDetails `json:"contentDetails"` // How many videos etc
 }
 
 type ContentDetails struct {
-	ItemCount int64 `json:"itemCount"` // How many videos in playlist
+	ItemCount uint64 `json:"itemCount"` // How many videos in playlist
 }
 
 type Snippet struct {
 	ChannelID    string             `json:"channelId"`    // Channel id
 	ChannelTitle string             `json:"channelTitle"` // Channel name
 	PublishedAt  time.Time          `json:"publishedAt"`
-	Title        string             `json:"title"` // Playlist name
-	Description  string             `json:"description"`
+	Title        string             `json:"title"`                 // Playlist name
+	Description  *string            `json:"description,omitempty"` // Playlist description
 	Thumbnails   *shared.Thumbnails `json:"thumbnails"`
 	Localized    *Localized         `json:"localized"` // might be different when using `hl` parameter in query?
 }
 
 type Localized struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
+	Title       string  `json:"title"`
+	Description *string `json:"description,omitempty"`
 }
 
 type Status struct {
